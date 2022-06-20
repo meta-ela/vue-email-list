ef28b1a9433bf010d4cacb97bc3035e0018b05f0
meta-ela <anxhelameta@live.it>
2022-06-20 18:17:35 +0200

    Adds axios, not working

--- main.js
+++ main.js
@@ -10,3 +10,31 @@ generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 */
+
+new Vue({
+    el: "#app",
+
+    data: {
+        email: null,
+        newEmails: [],
+    },
+
+    methods: {
+        generateRandomEmails(){
+            this.newEmails = [];
+
+            // creare un ciclo for per le email chiamando ajax
+            // dieci email random
+            for (let i = 0; i < 10; i++) {
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((axiosResp) => {
+                    this.eamil = axiosResp.data.response;
+                    this.newEmails.push(this.email);
+                    
+                });
+            }
+            console.log(newEmails);
+        }
+    },
+});
+
